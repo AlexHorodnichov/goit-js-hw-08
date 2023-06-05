4428712a03f69110283d5989e24919f89cf2aaef
AlexHorodnichov <116178502+AlexHorodnichov@users.noreply.github.com>
2023-06-05 08:29:53 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -18,6 +18,6 @@ listRef.insertAdjacentHTML('beforeend', createImgEl);
 
 let lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
-  captionDelay: 250,
+  captionDelay: 250, 
 });
  
